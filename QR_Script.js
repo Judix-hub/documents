768599ff0565275020cd17dac98e00e1c4197dd6
Judix-hub <judith.perez@vibia.com>
2025-05-21 07:57:04 +0200

    Create QR_Script.js

---
+++ QR_Script.js
@@ -0,0 +1,17 @@
+<div id="qrContainer"></div>
+
+<script src="https://judix-hub.github.io/documents/qr_cbites.js"></script>
+
+<script>
+const redirectPageURL = "https://join.vibia.com/app_vibia/download";
+
+// QR style
+new QRCode(document.getElementById("qrContainer"), {
+    text: redirectPageURL + "?qr_scanned=true",
+    width: 122,
+    height: 122,
+    colorDark: "#000000",
+    colorLight: "rgba(0,0,0,0)",
+    correctLevel: QRCode.CorrectLevel.H
+});
+</script>
